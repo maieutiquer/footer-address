9aab4668913fafec0dc919b3077b98745c74b015
daniel rodriguez <maieutiquer@gmail.com>
2014-06-24 14:51:18 +0300

    unify newline characters

--- mod_footeraddress/mod_footeraddress.php
+++ mod_footeraddress/mod_footeraddress.php
@@ -1,6 +1,6 @@
-<?php // no direct access
-defined( '_JEXEC' ) or die( 'Restricted access' );
-// Include the syndicate functions only once
-require_once( dirname(__FILE__).'/helper.php' );
-$footerInfo = modFooterAddressHelper::getFooterInfo( $params );
-require( JModuleHelper::getLayoutPath( 'mod_footeraddress' ) );
+<?php // no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+// Include the syndicate functions only once
+require_once( dirname(__FILE__).'/helper.php' );
+$footerInfo = modFooterAddressHelper::getFooterInfo( $params );
+require( JModuleHelper::getLayoutPath( 'mod_footeraddress' ) );
