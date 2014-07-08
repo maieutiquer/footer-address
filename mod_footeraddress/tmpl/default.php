e1d6b97c737a672c0a2c9b3a51a3659d50e7d0ad
daniel rodriguez <maieutiquer@gmail.com>
2014-07-08 12:47:05 +0300

    refactor view

--- mod_footeraddress/tmpl/default.php
+++ mod_footeraddress/tmpl/default.php
@@ -20,63 +20,68 @@ function printSeparator() {
   }
 }
 
-$showName = isset($footerInfo[0]);
-$showAddress = isset($footerInfo[1]) || isset($footerInfo[2]) || isset($footerInfo[3]) || isset($footerInfo[4]);
-  $showStreet = isset($footerInfo[1]);
-  $showPostalCode = isset($footerInfo[2]);
-  $showLocality = isset($footerInfo[3]);
-  $showCountry = isset($footerInfo[4]);
-$showMobile = isset($footerInfo[5]);
-$showLandline = isset($footerInfo[6]);
-$showFax = isset($footerInfo[7]);
-$showEmail = isset($footerInfo[8]);
-$showAdditional = isset($footerInfo[9]);
+$name = ($footerInfo[0]);
+$address = isset($footerInfo[1]) || isset($footerInfo[2]) || isset($footerInfo[3]) || isset($footerInfo[4]);
+  $street = ($footerInfo[1]);
+  $postalCode = ($footerInfo[2]);
+  $locality = ($footerInfo[3]);
+  $country = ($footerInfo[4]);
+$mobile = ($footerInfo[5]);
+$landline = ($footerInfo[6]);
+$fax = ($footerInfo[7]);
+$email = ($footerInfo[8]);
+$additional = ($footerInfo[9]);
+$description = ($footerInfo[10]);
 
 
 $markup.= '<address class="footer-address" itemscope itemtype="http://schema.org/Organization">';
 $separatorCounter--;
-if ($showName) {
-  $markup.= '<a id="homepage-link-footer" href="."><span itemprop="name">'.$footerInfo[0].'</span></a> ';
+if ($name) {
+  $markup.= '<a id="homepage-link-footer" href="."><span itemprop="name">'.$name.'</span></a> ';
   printSeparator();
 }
-if ($showAddress) {
+if ($description) {
+  $markup.= '<span class="footer-description">'.$description.'</span> ';
+  printSeparator();
+}
+if ($address) {
   $markup.= '<span id="postal-address" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">';
-  if ($showStreet) {
-    $markup.= '<span itemprop="streetAddress">'.$footerInfo[1].'</span> ';
+  if ($street) {
+    $markup.= '<span itemprop="streetAddress">'.$street.'</span> ';
     printSeparator();
   }
-  if ($showPostalCode) {
-    $markup.= '<span itemprop="postalCode">'.$footerInfo[2].'</span> ';
+  if ($postalCode) {
+    $markup.= '<span itemprop="postalCode">'.$postalCode.'</span> ';
     printSeparator();
   }
-  if ($showLocality) {
-    $markup.= '<span itemprop="addressLocality">'.$footerInfo[3].'</span> ';
+  if ($locality) {
+    $markup.= '<span itemprop="addressLocality">'.$locality.'</span> ';
     printSeparator();
   }
-  if ($showCountry) {
-    $markup.= '<span itemprop="addressCountry">'.$footerInfo[4].'</span>';
+  if ($country) {
+    $markup.= '<span itemprop="addressCountry">'.$country.'</span>';
   }
   $markup.= '</span> ';
   printSeparator();
 }
-if ($showMobile) {
-  $markup.= '<span itemprop="telephone">'.$footerInfo[5].'</span> ';
+if ($mobile) {
+  $markup.= '<span itemprop="telephone">'.$mobile.'</span> ';
   printSeparator();
 }
-if ($showLandline) {
-  $markup.= '<span itemprop="telephone">'.$footerInfo[6].'</span> ';
+if ($landline) {
+  $markup.= '<span itemprop="telephone">'.$landline.'</span> ';
   printSeparator();
 }
-if ($showFax) {
-  $markup.= '<span itemprop="faxNumber">'.$footerInfo[7].'</span> ';
+if ($fax) {
+  $markup.= '<span itemprop="faxNumber">'.$fax.'</span> ';
   printSeparator();
 }
-if ($showEmail) {
-  $markup.= '<a id="site-email-footer" href="mailto:'.$footerInfo[8].'"><span itemprop="email">'.$footerInfo[8].'</span></a> ';
+if ($email) {
+  $markup.= '<a id="site-email-footer" href="mailto:'.$email.'"><span itemprop="email">'.$email.'</span></a> ';
   printSeparator();
 }
-if ($showAdditional) {
-  $markup.= '<span>'.$footerInfo[9].'</span> ';
+if ($additional) {
+  $markup.= '<span class="footer-additional">'.$additional.'</span> ';
 }
 
 $markup.= '</address>';
